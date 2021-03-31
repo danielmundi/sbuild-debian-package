5f70b56c48d74181a1b4855296c51ae8c0ca8f8b
Daniel Finimundi <daniel@finimundi.com>
2021-03-31 20:59:29 -0300

    Avoid using chroot for permissionn issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 distro="${INPUT_DISTRO:-buster}"
 arch="${INPUT_ARCH:-armhf}"
 
@@ -7,32 +9,15 @@ export DEBIAN_FRONTEND=noninteractive
 
 #cd /profiler
 
-#dpkg --add-architecture ${arch}
+dpkg --add-architecture ${arch}
 
-#apt-get update -yqq
-#apt-get install -yqq build-essential crossbuild-essential-${arch}
+apt-get update -yqq
+apt-get install -yqq build-essential crossbuild-essential-${arch}
 
 #echo "Build dependencies"
 #apt-get build-dep -y -a${arch} ./
-#apt-get build-dep -y ./
-#mk-build-deps --install --host-arch ${arch} --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes debian/control
+mk-build-deps --install --host-arch ${arch} --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes debian/control
 
 #echo "Build package"
-#CONFIG_SITE=/etc/dpkg-cross/cross-config.${arch}  DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -a${arch} -Pcross,nocheck
+CONFIG_SITE=/etc/dpkg-cross/cross-config.${arch}  DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -a${arch} -Pcross,nocheck
 
-set -e
-# Create schroot
-sbuild-createchroot --arch=${arch} ${distro} \
-    /srv/chroot/${distro}-${arch}-sbuild http://deb.debian.org/debian
-
-# Generate .dsc file
-res=$(dpkg-source -b ./)
-
-# Get .dsc file name
-dsc_file=$(echo "$res" | grep .dsc | grep -o '[^ ]*$')
-
-# Build inside schroot
-sbuild --arch=${arch} -c ${distro}-${arch}-sbuild \
-    -d ${distro} ../${dsc_file}
-
-#OS=debian DIST=jessie ARCH=armhf pbuilder --build --pkgname-logfile --debbuildopts -B ../${dsc_file}
