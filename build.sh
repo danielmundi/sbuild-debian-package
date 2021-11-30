f10e93c0924eb95962ce67e85c4e8077443ea703
Daniel Finimundi <daniel@finimundi.com>
2021-11-30 10:55:06 -0300

    Load workaround binary to schroot
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- build.sh
+++ build.sh
@@ -35,23 +35,25 @@ schroot_target="/srv/chroot/${schroot_name}"
 # https://github.com/M-Reimer/repo-make/blob/master/repo-make-ci.sh#L252-L274
 # https://github.com/osrf/multiarch-docker-image-generation/issues/36
 # Start workaround
-if [ -x "/usr/bin/qemu-arm-static" ]; then
+if [ -x "${schroot_target}/usr/bin/qemu-arm-static" ]; then
   echo 'BUILD.SH CI: qemu-arm-static build --- implementing semtimedop workaround'
-  cat <<EOF > "/tmp/wrap_semop.c"
-#include <unistd.h>
-#include <asm/unistd.h>
-#include <sys/syscall.h>
-#include <linux/sem.h>
-/* glibc 2.31 wraps semop() as a call to semtimedop() with the timespec set to NULL
- * qemu 3.1 doesn't support semtimedop(), so this wrapper syscalls the real semop()
- */
-int semop(int semid, struct sembuf *sops, unsigned nsops)
-{
-  return syscall(__NR_semop, semid, sops, nsops);
-}
-EOF
-  sudo chroot "$schroot_target" gcc -fPIC -shared -o /opt/libpreload-semop.so /tmp/wrap_semop.c
-  echo '/opt/libpreload-semop.so' >> "/etc/ld.so.preload"
+  cat <<-EOF > "/tmp/wrap_semop.c"
+    #include <unistd.h>
+    #include <asm/unistd.h>
+    #include <sys/syscall.h>
+    #include <linux/sem.h>
+    /* glibc 2.31 wraps semop() as a call to semtimedop() with the timespec set to NULL
+     * qemu 3.1 doesn't support semtimedop(), so this wrapper syscalls the real semop()
+     */
+    int semop(int semid, struct sembuf *sops, unsigned nsops)
+    {
+      return syscall(__NR_semop, semid, sops, nsops);
+    }
+    EOF
+
+  sudo cp "/tmp/wrap_semop.c" "${schroot_target}/tmp/wrap_semop.c"
+  sudo schroot --chroot "${schroot_name}" --directory / gcc -fPIC -shared -o /opt/libpreload-semop.so /tmp/wrap_semop.c
+  echo '/opt/libpreload-semop.so' | sudo tee -a "${schroot_target}/etc/ld.so.preload"
   exit
 fi
 # End workaround
