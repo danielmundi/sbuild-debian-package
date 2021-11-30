2e86cd37c7b8b74dbb9ff1224fadf69a7e23c5fc
Daniel Finimundi <daniel@finimundi.com>
2021-11-30 11:31:30 -0300

    Get here-doc back
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- build.sh
+++ build.sh
@@ -37,19 +37,19 @@ schroot_target="/srv/chroot/${schroot_name}"
 # Start workaround
 if [ -x "${schroot_target}/usr/bin/qemu-arm-static" ]; then
   echo 'BUILD.SH CI: qemu-arm-static build --- implementing semtimedop workaround'
-  cat <<-EOF > "/tmp/wrap_semop.c"
-    #include <unistd.h>
-    #include <asm/unistd.h>
-    #include <sys/syscall.h>
-    #include <linux/sem.h>
-    /* glibc 2.31 wraps semop() as a call to semtimedop() with the timespec set to NULL
-     * qemu 3.1 doesn't support semtimedop(), so this wrapper syscalls the real semop()
-     */
-    int semop(int semid, struct sembuf *sops, unsigned nsops)
-    {
-      return syscall(__NR_semop, semid, sops, nsops);
-    }
-    EOF
+  cat <<EOF > "/tmp/wrap_semop.c"
+#include <unistd.h>
+#include <asm/unistd.h>
+#include <sys/syscall.h>
+#include <linux/sem.h>
+/* glibc 2.31 wraps semop() as a call to semtimedop() with the timespec set to NULL
+ * qemu 3.1 doesn't support semtimedop(), so this wrapper syscalls the real semop()
+ */
+int semop(int semid, struct sembuf *sops, unsigned nsops)
+{
+  return syscall(__NR_semop, semid, sops, nsops);
+}
+EOF
 
   sudo cp "/tmp/wrap_semop.c" "${schroot_target}/tmp/wrap_semop.c"
   sudo schroot --chroot "${schroot_name}" --directory / gcc -fPIC -shared -o /opt/libpreload-semop.so /tmp/wrap_semop.c
